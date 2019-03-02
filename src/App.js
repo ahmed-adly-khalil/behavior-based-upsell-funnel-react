73eea48295eba6876f9180ecc58c15774e39e200
Ahmed Adly <ahmedadly@gmail.com>
2019-03-02 12:18:00 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '@shopify/polaris/styles.css';
+import {AppProvider, Page, Card, Button} from '@shopify/polaris';
+
+class App extends Component {
+  render() {
+    return (
+      <AppProvider>
+      <Page title="application config">
+        <Card sectioned>
+          <Button onClick={() => alert('Button clicked! hahahahaha')}>Example button 1111</Button>
+        </Card>
+      </Page>
+    </AppProvider>
+    );
+  }
+}
+export default App;
